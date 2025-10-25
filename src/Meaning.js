acd86ea1730320e68b4c8da91dcb253760b3d777
Martina Scibilia <martina.scibilia@outlook.com>
2025-10-25 17:59:23 +0200

    Add Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -6,6 +7,7 @@ export default function Meaning(props) {
       <h4 className="fw-bold">{props.meaning.partOfSpeech}</h4>
       <p>{props.meaning.definition}</p>
       <p className="fst-italic">{props.meaning.example}</p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
