3619c258feb078cd30cc12675e65726814c88761
Martina Scibilia <martina.scibilia@outlook.com>
2025-10-26 17:39:19 +0100

    Refactor Search.js

--- src/Search.js
+++ src/Search.js
@@ -10,33 +10,40 @@ export default function Search() {
     setResults(response.data);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
+  function searchWord() {
     const apiKey = "733615547b11515efo464ab9111t0c1b";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchWord();
   }
 
   function getWord(event) {
     setWord(event.target.value);
   }
 
+  let searchForm = (
+    <section>
+      <h3 className="mt-3 mb-4">What word do you want to look for?</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a word..."
+          autoFocus="on"
+          onChange={getWord}
+          className="form-control"
+        ></input>
+      </form>
+      <p className="opacity-75 mt-4">e.g. book, sunset, forest</p>
+    </section>
+  );
+
   return (
     <div className="Search">
-      <section>
-        <h3 className="mt-3 mb-4">What word do you want to look for?</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            placeholder="Type a word..."
-            autoFocus="on"
-            onChange={getWord}
-            className="form-control"
-          ></input>
-        </form>
-        <p className="opacity-75 mt-4">e.g. book, sunset, forest</p>
-      </section>
+      {searchForm}
       <Results results={results} />
     </div>
   );
